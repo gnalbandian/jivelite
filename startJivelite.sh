1dd2232d851bbebb45c330be153530b6ccd8163d
gnalbandian <gonzalonalbandian@hotmail.com>
2016-07-07 19:37:32 -0300

    Create startJivelite.sh

---
+++ startJivelite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Change these if you changed your install path
+INSTALL_DIR=`pwd`
+LIB_DIR=$INSTALL_DIR/lib
+
+## Start up
+export LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
+export PATH=$PATH:$INSTALL_DIR/bin
+
+export SDL_TOUCHSCREEN=1
+export JIVE_NOCURSOR=1
+export TSLIB_CONFFILE=$INSTALL_DIR/lib/ts.conf
+export TSLIB_PLUGINDIR=$INSTALL_DIR/lib/ts
+export SDL_MOUSEDRV=TSLIB
+export TSLIB_TSDEVICE=/dev/input/event0
+
+$INSTALL_DIR/bin/jivelite
+
+exit 0;
